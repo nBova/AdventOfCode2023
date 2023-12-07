3a3d65fa13c2d9a79a993ca7da69e4ec07b79503
Nick Bova <npb7642@rit.edu>
2023-12-06 21:04:42 -0500

    Day 1 part 1

---
+++ AdventOfCode2023/AdventOfCode2023/Day1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2023
+{
+    public static class Day1
+    {
+        private static string inputPath = @"..\..\..\Assets\Day1Input.txt";
+
+        public static int SumCalibrationValues()
+        {
+            List<int> calValues = new List<int>();
+            var lines = File.ReadLines(inputPath);
+
+            foreach(var line in lines) // Literally O(n²) right here but it's fine... right?
+            {
+                List<int> intsInLine = new List<int>();
+
+                foreach (char c in line)  // just going char by char on each line... I really hate this
+                {
+                    if (Char.IsNumber(c))
+                    {
+                        intsInLine.Add((int)Char.GetNumericValue(c));
+                    }
+                }
+
+                int calVal = int.Parse(intsInLine[0].ToString() + intsInLine[intsInLine.Count-1].ToString());
+                calValues.Add(calVal);
+            }
+
+            return calValues.Sum();
+        }
+    }
+}
---
+++ AdventOfCode2023/AdventOfCode2023/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AdventOfCode2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Day 1 Part 1 Solution: " + Day1.SumCalibrationValues());
+        }
+    }
+}
