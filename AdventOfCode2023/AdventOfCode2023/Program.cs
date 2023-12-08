81457cc795d72f7c737937f9144676a2dbc4d9df
Nick Bova <npb7642@rit.edu>
2023-12-08 00:24:32 -0500

    Day 2 Part 2 complete

--- AdventOfCode2023/AdventOfCode2023/Program.cs
+++ AdventOfCode2023/AdventOfCode2023/Program.cs
@@ -8,7 +8,8 @@ namespace AdventOfCode2023
         {
             Console.WriteLine("Day 1 Part 1 Solution: " + Day1.SumCalibrationValues(false));
             Console.WriteLine("Day 1 Part 2 Solution: " + Day1.SumCalibrationValues(true));
-            Console.WriteLine("Day 2 Part 2 Solution: " + Day2.SumGameIDs());
+            Console.WriteLine("Day 2 Part 1 Solution: " + Day2.SumGameIDs());
+            Console.WriteLine("Day 2 Part 2 Solution: " + Day2.CalculateMinimumPower());
         }
     }
 }
