00ce5826c4a91adce6f63e33ada820d4d31d72c1
Nick Bova <npb7642@rit.edu>
2023-12-08 00:31:28 -0500

    Day 3 start

---
+++ AdventOfCode2023/AdventOfCode2023/Day3.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2023
+{
+    public static class Day3
+    {
+        private static string inputPath = @"..\..\..\Assets\Day3Input.txt";
+    }
+}
